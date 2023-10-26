14a923fc763789634bd9a7a550b245bedca802f9
rajarohan <139357615+rajarohan@users.noreply.github.com>
2023-10-26 20:18:59 +0530

    Update asgi.py

--- LittleLemon/LittleLemon/asgi.py
+++ LittleLemon/LittleLemon/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for LittleLemon project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
 
 import os
 
